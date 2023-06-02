a36341b73646ed923d8a75c601c40634707ff1e1
amiof <arm131313@gmail.com>
2023-06-02 15:50:10 +0330

    fix style for show in center divs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import CounterPage from './components/counter'
 function App() {
 
   return (
-    <div style={{ display: "flex", justifyContent: 'center',flexDirection:"column" }}>
+    <div style={{ display: "flex", justifyContent: 'center',flexDirection:"column", alignItems:"center" }}>
       <div style={{ marginTop: 0, position: "absolute", top: 0, }}><h1>redux tool kit</h1></div>
       <CounterPage></CounterPage>
       <Number></Number>
